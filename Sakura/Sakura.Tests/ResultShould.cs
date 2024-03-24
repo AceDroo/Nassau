cc6b04c9fd40db60a85d607ab85b074718c08929
AceDroo <44382889+AceDroo@users.noreply.github.com>
2024-03-24 12:21:56 +1100

    feat: Adds Result Object

---
+++ Sakura/Sakura.Tests/ResultShould.cs
@@ -0,0 +1,23 @@
+using FluentAssertions;
+
+namespace Sakura.Tests;
+
+[TestFixture]
+public class ResultShould
+{
+    [Test]
+    public void Return_Success_And_Is_Not_Error_When_Ok()
+    {
+        var result = Result<int, string>.Ok(1);
+        result.Success.Should().Be(1);
+        result.IsError.Should().BeFalse();
+    }
+
+    [Test]
+    public void Return_Error_And_Is_Error_When_Err()
+    {
+        var result = Result<int, string>.Err("An error has occured!");
+        result.Error.Should().Be("An error has occured!");
+        result.IsError.Should().BeTrue();
+    }
+}
---
+++ Sakura/Sakura/Result.cs
@@ -0,0 +1,25 @@
+namespace Sakura;
+
+public readonly struct Result<TSuccess, TError>
+{
+    public TSuccess Success { get; }
+    public TError Error { get; }
+    public bool IsError { get; }
+
+    private Result(TSuccess success)
+    {
+        IsError = false;
+        Success = success;
+        Error = default!;
+    }
+
+    private Result(TError error)
+    {
+        IsError = true;
+        Error = error;
+        Success = default!;
+    }
+
+    public static Result<TSuccess, TError> Ok(TSuccess success) => new(success);
+    public static Result<TSuccess, TError> Err(TError error) => new(error);
+}
