0c52a5e7751614076b6097d6eb6ca88e2af9b681
AceDroo <44382889+AceDroo@users.noreply.github.com>
2024-04-27 10:29:13 +1000

    chore: Adds tests for Unit class

---
+++ Sakura/Sakura.Tests/UnitShould.cs
@@ -0,0 +1,36 @@
+using FluentAssertions;
+using Sakura.Ranking;
+using Sakura.Status;
+using Sakura.Units;
+
+namespace Sakura.Tests;
+
+[TestFixture]
+public class UnitShould
+{
+    [Test]
+    public void Reduce_Health_When_Taking_Damage()
+    {
+        var identity = new Identity("Sakura", "Kinomoto", "Sakura", "Female", "Human", Rank.Rookie);
+        Stats stats = [new Stat("Health", 100, 100)];
+        var appearance = new Appearance(1, 1, 1, 1, 1);
+        var unit = new Unit(identity, stats, appearance);
+
+        unit.TakeDamage(10);
+
+        unit.CurrentHealth.Should().Be(90);
+    }
+
+    [Test]
+    public void Increase_Health_When_Healing()
+    {
+        var identity = new Identity("Sakura", "Kinomoto", "Sakura", "Female", "Human", Rank.Rookie);
+        Stats stats = [new Stat("Health", 90, 100)];
+        var appearance = new Appearance(1, 1, 1, 1, 1);
+        var unit = new Unit(identity, stats, appearance);
+
+        unit.Heal(10);
+
+        unit.CurrentHealth.Should().Be(100);
+    }
+}
