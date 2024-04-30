8f0db14d04f6de705e9cca9ceb90b5e169512c58
AceDroo <44382889+AceDroo@users.noreply.github.com>
2024-04-30 13:22:46 +1000

    feat: Adds DataBlackboard

---
+++ Sakura/Sakura.Tests/DataBlackboardShould.cs
@@ -0,0 +1,25 @@
+using FluentAssertions;
+
+namespace Sakura.Tests;
+
+[TestFixture]
+public class DataBlackboardShould
+{
+    [Test]
+    public void Set_KeyValue_Pair_And_Return_Correct_Value()
+    {
+        var blackboard = new DataBlackboard();
+
+        blackboard.SetData("TEST", 1);
+
+        blackboard.GetData<int>("TEST").Should().Be(1);
+    }
+
+    [Test]
+    public void Return_Default_If_No_Value_For_Given_Key_Is_Found()
+    {
+        var blackboard = new DataBlackboard();
+
+        blackboard.GetData<int>("TEST").Should().Be(0);
+    }
+}
---
+++ Sakura/Sakura/DataBlackboard.cs
@@ -0,0 +1,21 @@
+namespace Sakura;
+
+public class DataBlackboard
+{
+    private readonly Dictionary<string, object> _data = new();
+
+    public void SetData(string key, object value)
+    {
+        _data[key] = value;
+    }
+
+    public T? GetData<T>(string key)
+    {
+        if (_data.TryGetValue(key, out var value))
+        {
+            return (T) value;
+        }
+
+        return default;
+    }
+}
