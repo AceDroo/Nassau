b2ad6bb9a146c9e419bf5449a03ba8a42bf5f1b1
AceDroo <44382889+AceDroo@users.noreply.github.com>
2024-05-10 12:15:15 +1000

    refactor: Adds more methods for Result Object

--- Sakura/Sakura.Tests/Functional/ResultShould.cs
+++ Sakura/Sakura.Tests/Functional/ResultShould.cs
@@ -21,4 +21,70 @@ public class ResultShould
         result.Error.Should().Be("An error has occured!");
         result.IsError.Should().BeTrue();
     }
+
+    [Test]
+    public void Match_Should_Invoke_SuccessHandler_When_Result_Is_Success()
+    {
+        var result = Result<int, string>.Ok(42);
+        var actual = result.Match(
+            onSuccess: success => success.ToString(),
+            onError: error => throw new Exception("Should not be called")
+        );
+
+        actual.Should().Be("42");
+    }
+
+    [Test]
+    public void Match_Should_Invoke_ErrorHandler_When_Result_Is_Error()
+    {
+        var result = Result<int, string>.Err("Error");
+        var actual = result.Match(
+            onSuccess: success => throw new Exception("Should not be called"),
+            onError: error => error.ToUpper()
+        );
+
+        actual.Should().Be("ERROR");
+    }
+
+    [Test]
+    public void Map_Should_Transform_Success_Value_When_Result_Is_Success()
+    {
+        var result = Result<int, string>.Ok(42);
+        var mappedResult = result.Map(success => success * 2);
+
+        mappedResult.Should().BeEquivalentTo(Result<int, string>.Ok(84));
+    }
+
+    [Test]
+    public void Map_Should_Not_Transform_Error_Value_When_Result_Is_Error()
+    {
+        var result = Result<int, string>.Err("Error");
+        var map = result.Map<int>(_ => throw new Exception("Should not be called"));
+
+        map.Should().BeEquivalentTo(Result<int, string>.Err("Error"));
+    }
+
+    [Test]
+    public void Reduce_Should_Return_Success_Value_When_Result_Is_Success()
+    {
+        var result = Result<int, string>.Ok(42);
+        var reducedValue = result.Reduce(
+            onSuccess: success => success * 2,
+            onError: error => throw new Exception("Should not be called")
+        );
+
+        reducedValue.Should().Be(84);
+    }
+
+    [Test]
+    public void Reduce_Should_Return_Error_Value_When_Result_Is_Error()
+    {
+        var result = Result<int, string>.Err("Error");
+        var reducedValue = result.Reduce(
+            onSuccess: success => throw new Exception("Should not be called"),
+            onError: error => error.ToUpper()
+        );
+
+        reducedValue.Should().Be("ERROR");
+    }
 }
--- Sakura/TrenchCats.Functional/Result.cs
+++ Sakura/TrenchCats.Functional/Result.cs
@@ -1,5 +1,7 @@
 namespace TrenchCats.Functional;
 
+using System;
+
 public readonly struct Result<TSuccess, TError>
 {
     public TSuccess Success { get; }
@@ -22,4 +24,12 @@ public readonly struct Result<TSuccess, TError>
 
     public static Result<TSuccess, TError> Ok(TSuccess success) => new(success);
     public static Result<TSuccess, TError> Err(TError error) => new(error);
+
+    public TResult Match<TResult>(Func<TSuccess, TResult> onSuccess, Func<TError, TResult> onError) => 
+        IsError ? onError(Error) : onSuccess(Success);
+
+    public Result<TNewSuccess, TError> Map<TNewSuccess>(Func<TSuccess, TNewSuccess> map) => 
+        IsError ? Result<TNewSuccess, TError>.Err(Error) : Result<TNewSuccess, TError>.Ok(map(Success));
+
+    public T Reduce<T>(Func<TSuccess, T> onSuccess, Func<TError, T> onError) => IsError ? onError(Error) : onSuccess(Success);
 }
